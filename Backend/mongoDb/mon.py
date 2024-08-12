fcd74aa156a7263b368daad3907bde0873913c47
TalKehila <129615478+TalKehila@users.noreply.github.com>
2024-08-12 19:21:21 +0300

    added logic to mongoDB file and open clean main.py  file

--- Backend/mongoDb/mon.py
+++ Backend/mongoDb/mon.py
@@ -1,7 +1,29 @@
 from pymongo import MongoClient
+from fastapi import FastAPI, HTTPException
 
 
 class MongoDBService:
-    def init(self, uri, db_name):
-        self.client = MongoClient()
+    def __init__(self, uri, db_name):
+        self.client = MongoClient(uri)
         self.db = self.client[db_name]
+
+    def close(self):
+        self.client.close()
+
+
+db_service = MongoDBService("mongodb://localhost:27017", "PokemonUsers")
+db_service.close()
+
+
+def insert_user(self, user):
+    pass
+
+
+def get_user(self,user_id):
+    pass
+
+def update_user(self,user):
+    pass
+
+def delete_user(self,user):
+    pass
