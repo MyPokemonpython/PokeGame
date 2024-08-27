22ed18428555f34157064294f5f584ff57fc4586
TalKehila <tal.kehila10@gmail.com>
2024-08-27 19:04:22 +0300

    main.py ready before postman

--- Backend/mongoDb/mon.py
+++ Backend/mongoDb/mon.py
@@ -1,28 +1,23 @@
 from pymongo import MongoClient
 
+
 class MongoDBService:
     def __init__(self, uri, db_name):
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
-        self.collection = self.db["users"]  ## collection for user
+        self.collection = self.db["users"]  # Collection for user
 
+    def insert_user(self, user):
+        self.collection.insert_one(user.__dict__)
 
-db_service = MongoDBService("mongodb://localhost:27017", "PokemonUsers")
-db_service.close()
+    def get_user(self, user_id):
+        return self.collection.find_one({"user_id": user_id})
 
+    def update_user(self, user):
+        self.collection.update_one({"user_id": user.user_id}, {"$set": user.__dict__})
 
-def insert_user(self, user):
-    self.collection.insert_one(user.__dict__)
+    def delete_user(self, user_id):
+        self.collection.delete_one({"user_id": user_id})
 
-
-def get_user(self, user_id):
-    self.collection.find_one({"user_id": user_id})
-
-
-def update_user(self, user):
-    self.collection.update_one({"user_id": user.user_id}, {"$set": user.__dict__})
-
-
-def delete_user(self, user):
     def close(self):
         self.client.close()
